fb13ceb19748b185409fae1b8fda9e71f487efeb
cauly <cauly@caulyemily.info>
2021-01-25 09:47:56 +0800

    实现带泛型的UnionFind

--- leetcode-utils/src/union_find.rs
+++ leetcode-utils/src/union_find.rs
@@ -1,53 +1,172 @@
-pub struct UnionFind {
-    id: Vec<i32>,
-    size: Vec<i32>,
-    count: i32,
+use std::collections::HashMap;
+
+pub struct UnionFind4Usize {
+    id: Vec<usize>,
+    size: Vec<usize>,
+    count: usize,
+    length: usize,
 }
 
-impl UnionFind {
-    pub fn new(count: i32) -> Self {
-        UnionFind {
+pub struct UnionFind<T>
+where
+    T: std::cmp::Eq,
+    T: std::hash::Hash,
+{
+    map: HashMap<T, usize>,
+    uf: UnionFind4Usize,
+}
+
+impl UnionFind4Usize {
+    pub fn new(count: usize) -> Self {
+        UnionFind4Usize {
             count,
+            length: count,
             id: (0..count).collect(),
             size: vec![1; count as usize],
         }
     }
 
-    pub fn is_connected(&mut self, p: i32, q: i32) -> bool {
+    pub fn add(&mut self) -> usize {
+        self.count += 1;
+        self.id.push(self.length);
+        self.size.push(1);
+        self.length += 1;
+        self.length - 1
+    }
+
+    pub fn is_connected(&mut self, p: usize, q: usize) -> bool {
         self.find(p) == self.find(q)
     }
 
-    pub fn find(&mut self, p: i32) -> i32 {
+    pub fn find(&mut self, p: usize) -> usize {
         let mut p = p;
-        while p != self.id[p as usize] {
-            self.id[p as usize] = self.id[self.id[p as usize] as usize];
-            p = self.id[p as usize] as i32
+        while p != self.id[p] {
+            self.id[p] = self.id[self.id[p]];
+            p = self.id[p]
         }
-        p as i32
+        p
     }
 
-    pub fn union_node(&mut self, p: i32, q: i32) {
+    pub fn union(&mut self, p: usize, q: usize) {
         let i = self.find(p);
         let j = self.find(q);
         if i == j {
             return;
         }
-        if self.size[i as usize] < self.size[j as usize] {
-            self.id[i as usize] = j;
-            self.size[j as usize] += self.size[i as usize];
+        if self.size[i] < self.size[j] {
+            self.id[i] = j;
+            self.size[j] += self.size[i];
         } else {
-            self.id[j as usize] = i;
-            self.size[i as usize] += self.size[j as usize];
+            self.id[j] = i;
+            self.size[i] += self.size[j];
         }
         self.count -= 1;
     }
 
-    pub fn union_count(&self) -> i32 {
+    pub fn union_count(&self) -> usize {
         self.count
     }
 
-    pub fn node_size(&mut self, p: i32) -> i32 {
+    pub fn union_size(&mut self, p: usize) -> usize {
         let root = self.find(p);
-        return self.size[root as usize];
+        return self.size[root];
+    }
+
+    pub fn len(&self) -> usize {
+        self.length
+    }
+}
+
+impl<T> UnionFind<T>
+where
+    T: std::cmp::Eq,
+    T: std::hash::Hash,
+{
+    pub fn new() -> Self {
+        UnionFind {
+            map: HashMap::new(),
+            uf: UnionFind4Usize {
+                count: 0,
+                length: 0,
+                id: Vec::new(),
+                size: Vec::new(),
+            },
+        }
+    }
+
+    pub fn from_iter<I>(iter: I) -> UnionFind<T>
+    where
+        I: IntoIterator<Item = T>,
+    {
+        let mut map = HashMap::new();
+        let mut index = 0;
+        for item in iter.into_iter() {
+            map.insert(item, index);
+            index += 1;
+        }
+        let len = map.len();
+        UnionFind {
+            map,
+            uf: UnionFind4Usize::new(len),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.uf.len()
+    }
+
+    pub fn union_count(&self) -> usize {
+        self.uf.union_count()
+    }
+
+    pub fn union_size(&mut self, p: T) -> Option<usize> {
+        if let Some(index) = self.map.get(&p) {
+            let root_index = self.uf.find(*index);
+            Some(self.uf.union_size(root_index))
+        } else {
+            None
+        }
+    }
+
+    pub fn find(&mut self, p: T) -> Option<&T> {
+        if let Some(index) = self.map.get(&p) {
+            let root_index = self.uf.find(*index);
+            self._find_by_index(root_index)
+        } else {
+            None
+        }
+    }
+
+    pub fn union(&mut self, p: T, q: T) -> Result<usize, String> {
+        if let Some(pindex) = self.map.get(&p) {
+            if let Some(qindex) = self.map.get(&q) {
+                self.uf.union(*pindex, *qindex);
+                return Ok(self.uf.union_size(*pindex));
+            }
+        }
+        Err("p/q not found.".to_string())
+    }
+
+    pub fn is_connected(&mut self, p: T, q: T) -> Result<bool, String> {
+        if let Some(pindex) = self.map.get(&p) {
+            if let Some(qindex) = self.map.get(&q) {
+                return Ok(self.uf.find(*pindex) == self.uf.find(*qindex));
+            }
+        }
+        Err("p/q not found.".to_string())
+    }
+
+    pub fn add(&mut self, p: T) {
+        let index = self.uf.add();
+        self.map.insert(p, index);
+    }
+
+    fn _find_by_index(&self, index: usize) -> Option<&T> {
+        for (k, v) in self.map.iter() {
+            if *v == index {
+                return Some(k);
+            }
+        }
+        None
     }
 }
--- leetcode-utils/tests/union_find.rs
+++ leetcode-utils/tests/union_find.rs
@@ -3,9 +3,58 @@ use leetcode_utils::union_find::*;
 
 #[test]
 fn test1() {
-    let mut uf = UnionFind::new(4);
-    uf.union_node(1, 2);
-    uf.union_node(2, 3);
+    let mut uf = UnionFind4Usize::new(4);
+    uf.union(1, 2);
+    uf.union(2, 3);
     assert_eq!(2, uf.union_count());
     assert_eq!(1, uf.find(3));
+    assert_eq!(1, uf.find(2));
+    assert_eq!(1, uf.find(1));
+    assert_eq!(0, uf.find(0));
+    assert_eq!(3, uf.union_size(3));
+    assert_eq!(4, uf.len());
+}
+
+#[test]
+fn test2() {
+    let mut uf = UnionFind4Usize::new(0);
+    assert_eq!(0, uf.union_count());
+
+    assert_eq!(0, uf.add());
+    assert_eq!(1, uf.add());
+    uf.union(0, 1);
+
+    assert_eq!(1, uf.union_count());
+    assert_eq!(0, uf.find(0));
+    assert_eq!(0, uf.find(1));
+    assert_eq!(2, uf.union_size(1));
+    assert_eq!(2, uf.len());
+}
+
+#[test]
+fn test3() {
+    let mut uf = UnionFind::new();
+    uf.add(100);
+    uf.add(200);
+    uf.add(300);
+    assert!(uf.union(100, 200).is_ok());
+    assert!(uf.union(100, 400).is_err());
+    assert_eq!(Some(&100), uf.find(200));
+    assert_eq!(2, uf.union_count());
+    assert_eq!(Some(2), uf.union_size(100));
+}
+
+#[test]
+fn test4() {
+    let list = vec!["a", "b", "c"];
+    let mut uf = UnionFind::from_iter(list);
+    assert!(uf.union("a", "b").is_ok());
+    assert!(uf.union("c", "b").is_ok());
+    assert_eq!(Some(&"a"), uf.find("a"));
+    assert_eq!(Some(&"a"), uf.find("b"));
+    assert_eq!(Some(&"a"), uf.find("c"));
+    assert_eq!(1, uf.union_count());
+    assert_eq!(Some(3), uf.union_size("a"));
+    assert_eq!(Some(3), uf.union_size("b"));
+    assert_eq!(Some(3), uf.union_size("c"));
 }
